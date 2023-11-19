1628e9b84716fadb0c928b321180eaac8544200f
shraddhaM02 <shradhapokle02@gmail.com>
2023-11-19 21:51:16 +0530

    Updated the hover part in ActivityCircle.jsx file,icon part has to be corrected.

--- src/Components/ActivityCircle.jsx
+++ src/Components/ActivityCircle.jsx
@@ -1,16 +1,36 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 
 function ActivityCircle(props) {
+  const [isHovering, setHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setHovering(false);
+  };
+
   return (
     <>
-        <div className="grid bg-red-400 rounded-full w-32 h-32 place-content-center text-cente circle" id="circle">
-          <FontAwesomeIcon
-            className="text-6xl text-center circle-icon pl-14" id="circle-icon"
-            icon={props.icons}
-          />
-          <div className="">Lorem, ipsum dolor.</div>
-        </div>
+      <div
+        className="grid bg-red-400 rounded-full w-32 h-32 place-content-center text-cente circle"
+        id="circle"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <FontAwesomeIcon
+          className="text-6xl text-center circle-icon"
+          id="circle-icon"
+          icon={props.icons}
+        />
+        {
+          isHovering && (
+            <div className="grid w-[120%] h-[120%] bg-red-500 justify-center ml-[100%] -mt-10">Lorem, ipsum dolor.</div>
+          )
+        }
+      </div>
     </>
   );
 }
