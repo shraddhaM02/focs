a8cef87aa78b898fc5b0c5e6842d670ecbe25088
shraddhaM02 <shradhapokle02@gmail.com>
2023-11-21 21:16:05 +0530

    Contact.jsx file updated,not completed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram,faFacebook,faTwitter,faGit,faSnapchat,faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+  faGit,
+  faSnapchat,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
@@ -9,24 +16,104 @@ function Contact() {
       <div className="flex flex-col items-center p-6 space-y-4">
         <h1 className="text-2xl">Get in touch with us</h1>
         <div className="space-x-8">
-              <FontAwesomeIcon icon={faFacebook} size="xl"/>
-              <FontAwesomeIcon icon={faInstagram} size="xl" />
-              <FontAwesomeIcon icon={faTwitter} size="xl"/>
-              <FontAwesomeIcon icon={faGit} size="xl" />
-              <FontAwesomeIcon icon={faSnapchat} size="xl" />
-              <FontAwesomeIcon icon={faLinkedin} size="xl" />
+          <FontAwesomeIcon icon={faFacebook} size="xl" />
+          <FontAwesomeIcon icon={faInstagram} size="xl" />
+          <FontAwesomeIcon icon={faTwitter} size="xl" />
+          <FontAwesomeIcon icon={faGit} size="xl" />
+          <FontAwesomeIcon icon={faSnapchat} size="xl" />
+          <FontAwesomeIcon icon={faLinkedin} size="xl" />
         </div>
-        <div className="grid grid-cols-3">
-              <div>
-                     <h1>CONTACT US</h1>
-                     <div className="gap-3 space-x-2">
-                            <FontAwesomeIcon icon={faPhone} />
-                            <span>Call Us</span>
-                     </div>
-                     <span>9448668317,9611076613</span>
-              </div>
-              <div></div>
-              <div>2</div>
+        <div className="sm:grid sm:grid-cols-3 p-10">
+          <div>
+            <h1>CONTACT US</h1>
+            <div className="gap-3 space-x-2">
+              <FontAwesomeIcon icon={faPhone} />
+              <span>Call Us</span>
+            </div>
+            <span>9448668317,9611076613</span>
+            <div className="gap-3 space-x-2">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <span>Email</span>
+            </div>
+            <span>sankethkumar@gmail.com,shradhapokle02@gmail.com</span>
+            <div className="my-8">
+              <form className="w-4/3">
+                <div className="relative z-0 w-full mb-5 group">
+                  <input
+                    type="email"
+                    name="floating_email"
+                    id="floating_email"
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    htmlFor="floating_email"
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Email address
+                  </label>
+                </div>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="text"
+                      name="floating_first_name"
+                      id="floating_first_name"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      htmlFor="floating_first_name"
+                      className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                      Name
+                    </label>
+                  </div>
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="text"
+                      name="floating_last_name"
+                      id="floating_last_name"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      htmlFor="floating_last_name"
+                      className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    >
+                      USN
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-full">
+                  <label
+                    htmlFor="message"
+                    className="peer-focus:font-medium block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="about"
+                      name="about"
+                      rows={5}
+                      className="block w-[95%] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring- my-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Suggestion.jsx
+++ src/Components/Suggestion.jsx
@@ -34,26 +34,26 @@ function Suggestion() {
           />
         </div>
 
-        <div className="col-span-full">
-          <label
-            for="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Suggestions
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="about"
-              name="about"
-              rows={5}
-              className="block w-[80%] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              defaultValue={""}
-            />
+          <div className="col-span-full">
+            <label
+              for="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Suggestions
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="about"
+                name="about"
+                rows={5}
+                className="block w-[80%] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-300">
+              Give Your valuable Suggestion.
+            </p>
           </div>
-          <p className="mt-3 text-sm leading-6 text-gray-300">
-            Give Your valuable Suggestion.
-          </p>
-        </div>
 
         <button
           type="submit"
